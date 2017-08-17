7fc3857ad415cc7d6ecadbf1a05ec2ab1811eca2
christophrrb <christopherballenger@gmail.com>
2017-08-17 14:16:23 -0400

    First commit.

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags always come first -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>DECA Programming Club!</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+  <!--My CSS-->
+  <link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+
+<body>
+  <!--Header-->
+  <div class="centered-text">
+    <!--Needs coding background and text highlighting to be readable.
+        Add JS Typing Thing-->
+    <h1 class="monospace center-text">&lt;DPC /&gt;</h1>
+    <i><p class="monospace center-text">&lt;DECA Programming Club&gt;</p></i>
+  </div>
+
+  <!--Picture might go better here actually.-->
+
+  <!--What We'll Cover-->
+  <div class="center-text">
+    <h2 class="monospace">What We'll Cover</h1>
+    <figure class="figure">
+      <img src="media/html.png" class="figure-img img-fluid img-rounded icon" alt="HTML">
+      <figcaption class="figure-caption">HTML</figcaption>
+    </figure>
+    <figure class="figure">
+      <img src="media/css.png" class="figure-img img-fluid img-rounded icon" alt="CSS">
+      <figcaption class="figure-caption">CSS</figcaption>
+    </figure>
+    <figure class="figure">
+      <img src="media/php.svg" class="figure-img img-fluid img-rounded icon" alt="PHP">
+      <figcaption class="figure-caption">PHP</figcaption>
+    </figure>
+    <figure class="figure">
+      <img src="media/mysql.png" class="figure-img img-fluid img-rounded icon" alt="MySQL">
+      <figcaption class="figure-caption">MySQL</figcaption>
+    </figure>
+  </div>
+
+  <!--CodeDay-->
+  <div style="relative">
+    <img style="margin-left: 20px; margin-top: 80px; position: absolute; z-index: 2; width: 375px; height: 175px;" src="media/codedaypicture.png" class="img-fluid pull-xs-left" alt="CodeDay">
+    <video style="position: absolute; z-index: 1; width: 1536px; height: 864px;" preload="" muted="" autoplay="" loop="">
+            <source src="media/codedayvideo.mp4" type="video/mp4">
+    </video>
+    <ul style="margin-left: 20px; margin-top: 250px; position: absolute; z-index: 2;">
+      <li style="color: white" class="figure-caption">Fun</li>
+      <li style="color: white" class="figure-caption">24 Hours</li>
+      <li style="color: white" class="figure-caption">Meet People</li>
+      <li style="color: white" class="figure-caption">Eat Well</li>
+    </ul>
+  </div>
+
+  <!--Requirements-->
+  <div>
+  </div>
+  <div> <!--Extra jQuery and Bootstrap stuff-->
+    <!-- jQuery first, then Bootstrap JS. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+  </div>
+</body>
+
+</html>
