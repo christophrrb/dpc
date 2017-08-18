4f57d84fa3e06242097a9895ae1ed377149aff6a
christophrrb <christopherballenger@gmail.com>
2017-08-18 00:29:09 -0400

    Removed the 'Connected successfully!' message.

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@ $conn = new mysqli("localhost", "root", "", "dpc");
 if ($conn->connect_error) {
   die ("Connection Error: " . $conn->connect_error);
 } else {
-  echo "Connected successfully!";
+  
 }
 
 ?>
